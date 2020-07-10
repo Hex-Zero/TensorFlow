d5946ef8efb45cfb250cc6eee6e2d923c0a7ff3a
Ace Alrich <hex0@live.com>
2020-07-10 07:01:19 +0100

    train data

--- Main/__init__.py
+++ Main/__init__.py
@@ -3,3 +3,8 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
+data = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = data.load_data()
+
+print(train_labels[6])
