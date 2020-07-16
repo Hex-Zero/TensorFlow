3eae151354923db97615ff342a8e9b5857b6c63f
Ace Alrich <hex0@live.com>
2020-07-16 21:05:56 +0100

    shrink data

--- Main/__init__.py
+++ Main/__init__.py
@@ -10,5 +10,8 @@ data = keras.datasets.fashion_mnist
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-plt.imshow(train_images[7])
+train_images = train_images/255.0
+test_images = test_images/255.0
+
+plt.imshow(train_images[7], cmap=plt.cm.binary)
 plt.show()
