3712bfc613594f5eb439baf24f189ff05bfe775d
Ace Alrich <hex0@live.com>
2020-07-23 20:46:11 +0100

    easy image recognition

--- Main/__init__.py
+++ Main/__init__.py
@@ -21,10 +21,15 @@ model = keras.Sequential([
 ])
 model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 
-model.fit(train_images, train_labels, epochs=20)
+model.fit(train_images, train_labels, epochs=1)
 
 prediction = model.predict(test_images)
-print(prediction)
+for i in range(5):
+    plt.grid(False)
+    plt.imshow(test_images[i], cmap=plt.cm.binary)
+    plt.xlabel("Actual: " + class_names[test_labels[i]])
+    plt.title("Prediction: " + class_names[np.argmax(prediction[i])])
+    plt.show()
 
 # test_loss, test_acc = model.evaluate(test_images, test_labels)
 #
